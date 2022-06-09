687c9b2321d023a5a8a34e060921d326c9631518
James Coffman <jamesc4023@gmail.com>
2022-06-09 16:41:07 -0400

    commit tree a7b3c3fa5f7240977b92b4505c0a6ad203713373

---
+++ app/serializers/authors_posts_serializer.rb
@@ -0,0 +1,10 @@
+class AuthorsPostsSerializer < ActiveModel::Serializer
+  attributes :title, :short_content
+
+  has_many :post_tags
+  has_many :tags
+
+  def short_content
+    "#{object.content[0..39]...}"
+  end
+end
